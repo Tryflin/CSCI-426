2ac1582741d1b533ec1551cb918ab45f69b57c19
daneboyd20001 <daneboyd20001@gmail.com>
2026-04-21 14:04:06 -0500

    Added Login Pages with CSS and JS

---
+++ loginPage.js
@@ -0,0 +1,8 @@
+function OnLogin(){
+    const username = document.getElementById("user").value;
+    const password = document.getElementById("pass").value;
+    const rememberMe = document.getElementById("rememberMe").checked;
+
+    console.log({username, password, rememberMe})
+    //TODO: Impliment DBA VERIFICATION AND REGISTRATION
+}
---
+++ signupPage.js
@@ -0,0 +1,7 @@
+function OnSignup(){
+    const username = document.getElementById("user").value;
+    const password = document.getElementById("pass").value;
+
+    console.log({username, password})
+    //TODO: Impliment DBA VERIFICATION AND REGISTRATION
+}
